2a86e475b1dbc53472291622b570799e23079e21
galle <galle@DESKTOP-J6RSNG6>
2016-06-06 10:11:36 +0200

    - Metodo "operacion" creado. - Resto del codigo extraido y metido en el metodo "operacion".

--- src/examenfinalparte2/Metodos.java
+++ src/examenfinalparte2/Metodos.java
@@ -17,5 +17,51 @@ public class Metodos {
             System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
         }
     }
+    public void operacion (int dig, int ndig, boolean p) {
+        for (int i = 1; i <= 99999; i++) {
+            int aux = i;
+
+            int contador = 0;
+
+            while (aux != 0) {
+                aux = aux / 10;
+                contador++;
+            }
+            ndig = contador;
+
+            if (ndig == dig) {
+                if (i < 4) {
+                    p = true;
+                } else if (i % 2 == 0) {
+                    p = false;
+                } else {
+                    int contador1 = 0;
+                    int i1 = 1;
+                    int k = (i - 1) / 2;
+                    if (k % 2 == 0) {
+                        k--;
+                    }
+
+                    while (i1 <= k) {
+                        if (i % i1 == 0) {
+                            contador1++;
+                        }
+                        i1 += 2;
+                        if (contador1 == 2) {
+                            i1 = k + 1;
+                        }
+                    }
+
+                    if (contador1 == 1) {
+                        p = true;
+                    }
+                }
+
+                if (p == true) {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
     
 }
