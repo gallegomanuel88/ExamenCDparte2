92b536aedbe047d58d41bfa1a8aafae4ee87f6f7
galle <galle@DESKTOP-J6RSNG6>
2016-06-06 10:03:08 +0200

    - Clase Metodos creada. - Metodo "condicion" creado en la clase Metodos. Este metodo contiene el primer if que imprime por pantalla.

---
+++ src/examenfinalparte2/Metodos.java
@@ -0,0 +1,13 @@
+package examenfinalparte2;
+
+/**
+ *
+ * @author gallegomanuel88
+ */
+public class Metodos {
+    public void condicion (int valor){
+        if (valor <= 0) {
+            System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
+        }
+    }
+}
